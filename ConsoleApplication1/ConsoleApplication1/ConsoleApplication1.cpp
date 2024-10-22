c0a9bc624c6b22dc29fa92ba0a224c23bcc570eb
Mathieu <owymatt3@gmail.com>
2024-10-22 14:36:41 +0200

    blockchain

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+
+int main()
+{
+    int age = 52;
+    std::string prenom = "Matt";
+    std::cout << "Hello World!\n" <<age<< std::endl;
+    std::cout << age << std::endl;
+    std::cin>> prenom;
+}
